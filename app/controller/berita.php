43f8323b2f91cadef1d1f97a6d1ac94a41a69a4f
pukiscuy <pukiscuy@gmail.com>
2015-05-21 16:45:04 +0700

    template front end

---
+++ app/controller/berita.php
@@ -0,0 +1,29 @@
+<?php
+
+class berita extends Controller {
+	
+	var $models = FALSE;
+	var $view;
+
+	
+	function __construct()
+	{
+		global $basedomain;
+		$this->loadmodule();
+		$this->view = $this->setSmarty();
+		$this->view->assign('basedomain',$basedomain);
+    }
+	
+	function loadmodule()
+	{
+        $this->contentHelper = $this->loadModel('contentHelper');
+	}
+	
+	function index(){
+		
+		return $this->loadView('berita/page_berita');
+    }
+
+}
+
+?>

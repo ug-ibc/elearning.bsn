805dc18ca27e78dd169d7068ae56be8c1ed8021b
Ovan Pulu <ovan89@gmail.com>
2015-05-27 12:52:39 +0700

    alter tabel generate soal

--- db/tbl_generate_soal.sql
+++ db/tbl_generate_soal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 27, 2015 at 09:38 AM
+-- Generation Time: May 27, 2015 at 12:52 PM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.5
 
@@ -33,10 +33,13 @@ CREATE TABLE IF NOT EXISTS `tbl_generate_soal` (
   `idUser` int(11) NOT NULL,
   `soal` text NOT NULL,
   `generate_date` datetime NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `finish` int(11) NOT NULL DEFAULT '0',
   `n_status` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idKursus` (`idKursus`,`idMateri`,`idUser`,`n_status`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=120 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

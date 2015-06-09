1a3e80870d9eff20b70de4f5d926788728f12ad0
andreassbayu <andreass.bayu@gmail.com>
2015-06-09 17:32:35 +0700

    kursus front

--- app/model/mkursus.php
+++ app/model/mkursus.php
@@ -16,7 +16,33 @@ class mkursus extends Database{
 
 	function getAllCourse($id)
 	{
+		$query = "SELECT * FROM grup_kursus WHERE idGrup_kursus = '{$id}' AND n_status = '1'";
+		$grup = $this->fetch($query);
 
+		$query = "SELECT * FROM kursus WHERE idGrup_kursus = '{$id}' AND n_status = '1'";
+		$course = $this->fetch($query,1);
+
+		foreach ($course as $key => $value) {
+			$query = "SELECT * FROM materi WHERE idKursus='{$value['idKursus']}' AND idGrup_kursus = '{$id}' AND n_status = '1'";
+			$materi = $this->fetch($query,1);
+			if($materi){
+				foreach ($materi as $key2 => $value) {
+					$query = "SELECT * FROM file WHERE n_status = '1' AND idMateri = '{$value['idMateri']}' AND idKursus='{$value['idKursus']}' AND idGrup_kursus = '{$id}'";
+					$file = $this->fetch($query,1);
+
+					$materi[$key2]['file'] = $file;
+					
+				}
+
+				$course[$key]['materi'] = $materi;
+			}
+			
+			
+		}
+		
+		$dataArr = array('grup' => $grup , 'course' => $course);
+		
+		return $dataArr;
 	}
 }
 ?>

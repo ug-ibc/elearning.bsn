337510712ec1b91c7815a04dae56b28d95e3c1cb
Ovan Pulu <ovan89@gmail.com>
2015-06-23 14:27:49 +0700

    register validasi

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -127,3 +127,19 @@ $(document).on('click','#finishQuiz', function(){
 	},"JSON")
 	
 })
+
+
+function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}  
+
+function compare(data1, data2)
+{
+	if (data1==data2) return true;
+	return false;
+}
+
+function validateNumber(evt){
+     evt.value = evt.value.replace(/[^0-9]/g,"");
+}

c2c5d69613deb5e031020b7f1412558228e2ec53
ilmawan <ilmawan.muharam@gmail.com>
2015-08-03 17:51:16 +0700

    update status buat sertificat

--- app/controller/search.php
+++ app/controller/search.php
@@ -85,11 +85,14 @@ class search extends Controller {
 		$tanggal = $ex2[0].'/'.$ex1[1].'/'.$ex1[0];
 		
 		if($certificate[nilai] >= $criteria[kategoriBaik]){
-			$kategori = "Baik";
+			// $kategori = "Baik";
+			$kategori = "Sangat Baik";
 		}elseif($certificate[nilai] >= $criteria[kategoriCukup] && $certificate[nilai] < $criteria[kategoriBaik]){
-			$kategori = "Cukup";
+			// $kategori = "Cukup";
+			$kategori = "Baik";
 		}else{
-			$kategori = "Kurang";
+			// $kategori = "Kurang";
+			$kategori = "";
 		}
 		$background_certificate =  $basedomain."assets/img/certificate/bg.jpg";
 		$this->reportHelper =$this->loadModel('reportHelper');
